6a02a3482b7e8ca6f845a8208fc08d89eeaebb04
TanishaMahapatra2 <85362508+TanishaMahapatra2@users.noreply.github.com>
2021-06-04 22:45:33 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+console.log(" My name is Tanisha ")
+ 
+var FirstName="Olivia";
+var LastName="Rodrigo";
+var FullName=FirstName.concat(LastName);
+console.log(FullName);
+Upper_case_name=FirstName.toUpperCase();
+console.log(Upper_case_name);
+Lower_case_name=LastName.toLowerCase();
+console.log(Lower_case_name);
+
+var n1=120;
+var n2=987;
+var sum=Number(n1) + Number(n2);
+console.log(sum);
+
+var Alphabets=["d","s","a","z","o"];
+console.log(Alphabets.sort());
+Alphabets.push("j","k");
+console.log(Alphabets);
+Alphabets.pop;
+console.log(Alphabets);
+Alphabets.unshift("x","p");
+console.log(Alphabets);
+Alphabets.shift("f");
+console.log(Alphabets);
+
+
+
+
+
+
+
+var replace_sentence="My favourite singer is Ariana Grande";
+console.log(replace_sentence);
+console.log (replace_sentence.replace( "Ariana Grande", "Taylor Swift"));
+
+console.log(Math.max(3467,3490));
+console.log(Math.min(9008,8009));
+console.log(Math.sqrt(400));
+console.log(Math.pow(80,2));
+
+console.log(replace_sentence.length);
+
+
